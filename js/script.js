1706c7bb3969bc66220e09c3c337af080c7b4c62
divye <divye.chakshu@gmail.com>
2015-03-22 01:35:07 -0300

    Final UI

--- js/script.js
+++
@@ -1,5 +0,0 @@
-var changeInput = document.querySelector('.js-check-change');
-
-changeInput.onchange = function() {
-  document.getElementById('js-display-change').innerHTML = changeInput.value;
-};
