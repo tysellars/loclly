9846c6280598b55c6ad9e0da475d306cdc94f9ac
divye <divye.chakshu@gmail.com>
2015-03-22 04:21:18 -0300

    Implementing Tags

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,20 +11,33 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+            var location = $("input#location").val();
+            var date = $("input#date").val();
+            var price = $("input#price").val();
             var message = $("textarea#message").val();
+            var hashTags = $("textarea#hashTags").val();
+            var allTags = "";
+            if(hashTags.indexOf(" ") > 0){
+                allTags = hashTags.split(" ");
+            }
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "./mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
-                    message: message
+                    phone: phone,
+                    eventAddress: eventAddress,
+                    eventDate: eventDate,
+                    eventPrice: eventPrice,
+                    message: message,
+                    hashTags: hashTags,
+                    allTags: allTags,
                 },
                 cache: false,
                 success: function() {
@@ -33,7 +46,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your event has been Created. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
